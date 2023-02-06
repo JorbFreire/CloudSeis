3d7ed21865599cbfe6f0b148760c60965c5017f2
JorbFreire <jorbfreire@gmail.com>
2023-02-06 19:53:42 -0300

    fix missing html file and change plot container page

--- webapp/src/views/Project/index.tsx
+++ webapp/src/views/Project/index.tsx
@@ -1,4 +1,5 @@
-import { useState, useEffect, useCallback } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import { useNavigate } from '@tanstack/react-location'
 import { Container, FileInput, StartButton } from './styles'
 
@@ -9,39 +10,19 @@ interface IProjectProps {
 export default function Project({ projectName }: IProjectProps) {
   const navigate = useNavigate()
   const [SUFiles, setSUFiles] = useState<FileList | null>()
-  const [SUFileInBase64, setSUFileInBase64] = useState<string>()
   const [readingFile, setReadingFile] = useState(false)
 
-  const getBase64 = (file: File) => {
-    const reader = new FileReader();
-    reader.readAsDataURL(file);
-
-    reader.onload = () => {
-      console.log('file reader load finished')
-      if (typeof reader.result == 'string')
-        return console.error(`result on file reading came with type: ${typeof reader.result}`)
-
-      // Converting to String to String becouse of a typescript error.
-      const base64String = String(reader.result)
-        .replace('data:', '')
-        .replace(/^.+,/, '');
-      setSUFileInBase64(base64String)
-      setReadingFile(false)
-    };
-    reader.onerror = (error) => {
-      console.error('Error: ', error);
-      setReadingFile(false)
-    };
-  }
-
-  const getBase64CallBack = useCallback(getBase64, [setSUFileInBase64, setReadingFile])
 
   const openDataWindow = () => navigate({ to: '/seismic-visualization/0' })
 
   useEffect(() => {
     if (SUFiles && !readingFile) {
       setReadingFile(true)
-      getBase64CallBack(SUFiles[0])
+      try {
+        axios.get('https://localhost:3001')
+      } catch (error) {
+
+      }
     }
   }, [SUFiles])
 
