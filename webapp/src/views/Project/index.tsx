f732cef0dd27321f70e9ad5efc669d90e123eeaf
edurs2602 <edurs.2602@gmail.com>
2023-02-13 18:42:31 -0300

    TreeView MUI

--- webapp/src/views/Project/index.tsx
+++ webapp/src/views/Project/index.tsx
@@ -3,8 +3,18 @@ import api from '../../services/api'
 import { useNavigate } from '@tanstack/react-location'
 import { Container, FileInput, Button } from './styles'
 
+import TreeView from '@mui/lab/TreeView';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import TreeItem from '@mui/lab/TreeItem';
+import ButtonMUI from '@mui/material/Button'
+
 interface IProjectProps {
   projectName?: string
+}
+
+interface ITreeline {
+  id: string
 }
 
 export default function Project({ projectName }: IProjectProps) {
@@ -12,6 +22,7 @@ export default function Project({ projectName }: IProjectProps) {
   const [SUFiles, setSUFiles] = useState<FileList | null>()
   const [lastFileName, setLastFileName] = useState("")
   const [loading, setLoading] = useState(false)
+  const [treelines, setTreelines] = useState<Array<ITreeline>>([])
 
   const openDataWindow = () => navigate({ to: `/seismic-visualization/${lastFileName}` })
 
@@ -38,8 +49,44 @@ export default function Project({ projectName }: IProjectProps) {
     }
   }
 
+  const treeView = () => {
+    const tempTreelines = treelines
+    tempTreelines.push({id: `${Math.random()}`})
+
+    setTreelines([...tempTreelines])
+  }
+
   return (
+  
     <Container onSubmit={submitFiles}>
+
+      <TreeView
+        aria-label="file system navigator"
+        defaultCollapseIcon={<ExpandMoreIcon />}
+        defaultExpandIcon={<ChevronRightIcon />}
+        sx={{ height: 240, flexGrow: 1, maxWidth: 400, overflowY: 'auto' }}
+      >
+
+        <Button onClick={treeView}>
+          Create Line
+        </Button>
+
+          {treelines.map((treeline) => (
+            <TreeItem nodeId={treeline.id} label={`Line ${treeline.id}`}>
+              <Button className='workFlowButton'>Create Workflow</Button>
+            </TreeItem>
+          ))}
+        
+
+
+        <TreeItem nodeId="5" label="Documents">
+          <TreeItem nodeId="10" label="OSS" />
+          <TreeItem nodeId="6" label="MUI">
+            <TreeItem nodeId="8" label="index.js" />
+          </TreeItem>
+        </TreeItem>
+      </TreeView>
+
       <h1>{projectName}</h1>
       <FileInput
         type="file"
