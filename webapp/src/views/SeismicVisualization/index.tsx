16c6d0d5ec04a67db19e9d48268af086e3c4d73b
JorbFreire <jorbfreire@gmail.com>
2023-02-13 15:49:15 -0300

    get and show plot

--- webapp/src/views/SeismicVisualization/index.tsx
+++ webapp/src/views/SeismicVisualization/index.tsx
@@ -1,4 +1,5 @@
-import { useEffect, useRef } from 'react'
+import { useState, useEffect } from "react"
+import api from '../../services/api'
 import { Container } from "./styles"
 
 interface SeismicVisualizationProps {
@@ -6,27 +7,33 @@ interface SeismicVisualizationProps {
 }
 
 export default function SeismicVisualization({ fileKey }: SeismicVisualizationProps) {
-  const iframeRef = useRef<HTMLIFrameElement>(null);
-  const SUFileAsBase64String = "thats not a base 64 string but should be"
+  const [plotDiv, setPlotDiv] = useState("")
+  const [plotScript, setPlotScript] = useState("")
 
-  // this file will be avaliable globaly inside the iframe 
-  const declareSUFileToIframe = (event: any) => {
-    const newScriptTag = document.createElement('script');
-    const SUFileDeclarationScript = document.createTextNode(`var SUFileAsBase64String = "${SUFileAsBase64String}"`);
-    newScriptTag.appendChild(SUFileDeclarationScript)
-    event.target.contentWindow.document.querySelector("body").prepend(newScriptTag)
-  }
+  useEffect(() => {
+    const getPlotHTML = async () => {
+      try {
+        const response = await api.get('/get-plot')
+        setPlotDiv(response.data.div)
+        setPlotScript(response.data.script)
+      } catch (error) {
+        console.error(error)
+      }
+    }
+    getPlotHTML()
+  }, [])
+
+  useEffect(() => {
+    const plotScriptContent = plotScript
+      .replace('<script type="text/javascript">', '')
+      .replace('</script>', '')
+    new Function(plotScriptContent)()
+  }, [plotDiv])
 
   return (
     <Container>
       <h1>Key: {fileKey}</h1>
-      <iframe
-        ref={iframeRef}
-        name="frame-id"
-        src="http://127.0.0.1:5173/src/pyscript/index.html"
-        onLoad={declareSUFileToIframe}
-      >
-      </iframe>
+      <div dangerouslySetInnerHTML={{ __html: plotDiv }} />
     </Container>
   )
 }
