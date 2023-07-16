27b3fbfb72743d9a733192c06ae60a3298982928
JorbFreire <jorbfreire@gmail.com>
2023-07-16 02:36:59 -0300

    refactor: implement repository pattern

--- server/app/main.py
+++ server/app/main.py
@@ -1,15 +1,17 @@
-import os
-from datetime import datetime
+
 from flask import Flask, send_file, request, jsonify
 from flask_cors import CORS
 
-from .plot import getPlot
-from .runSeismicUnix import runSeismicUnix
+from .repositories.PlotRepository import PlotRepository
+from .repositories.SeismicFileRepository import SeismicFileRepository
 
 app = Flask(__name__)
 CORS(app)
 
 app.config['UPLOAD_FOLDER'] = "static"
+
+plotRepository = PlotRepository()
+seismicFileRepository = SeismicFileRepository()
 
 @app.route("/su-file", methods=['GET'])
 def showSuFile():
@@ -18,30 +20,28 @@ def showSuFile():
     except Exception as e:
         return str(e)
 
+
 @app.route("/su-file", methods=['POST'])
 def createSuFile():
     file = request.files['file']
-
-    unique_filename = file.filename.replace(".su", "_").replace(" ", "_")
-    unique_filename = unique_filename + datetime.now().strftime("%d%m%Y_%H%M%S") + ".su"
-
-    file.save(os.path.join(app.config['UPLOAD_FOLDER'], unique_filename))
+    unique_filename = seismicFileRepository.create(file, app)
     return { "unique_filename": unique_filename }
 
-@app.route("/get-plot/<unique_filename>", methods=['GET'])
-def showPlotHtmlTags(unique_filename):
-    script, div = getPlot(unique_filename)
-    return jsonify({
-        "script": script,
-        "div": div
-    })
 
 @app.route("/su-file/<unique_filename>/filters", methods=['PUT'])
 def updateSuFile(unique_filename):
     data = request.get_json()
     updateOptions = data["updateOptions"]
-    runSeismicUnix(unique_filename, updateOptions)
+    process_output = seismicFileRepository.update(unique_filename, updateOptions)
     return jsonify({
-        "status": "file updated"
+        "process_output": process_output
     })
  
+
+@app.route("/get-plot/<unique_filename>", methods=['GET'])
+def showPlotHtmlTags(unique_filename):
+    script, div = plotRepository.show(unique_filename)
+    return jsonify({
+        "script": script,
+        "div": div
+    })
