00b07b09d747f8d0e76f7253f535ba3d7ed91097
JorbFreire <jorbfreire@gmail.com>
2023-07-16 14:29:11 -0300

    eject routes

--- server/app/main.py
+++ server/app/main.py
@@ -1,47 +1,11 @@
-
-from flask import Flask, send_file, request, jsonify
+from flask import Flask
 from flask_cors import CORS
 
-from .repositories.PlotRepository import PlotRepository
-from .repositories.SeismicFileRepository import SeismicFileRepository
+from .routes import router
 
 app = Flask(__name__)
 CORS(app)
 
 app.config['UPLOAD_FOLDER'] = "static"
 
-plotRepository = PlotRepository()
-seismicFileRepository = SeismicFileRepository()
-
-@app.route("/su-file", methods=['GET'])
-def showSuFile():
-    try:
-        return send_file('../static/marmousi_CS.su')
-    except Exception as e:
-        return str(e)
-
-
-@app.route("/su-file", methods=['POST'])
-def createSuFile():
-    file = request.files['file']
-    unique_filename = seismicFileRepository.create(file, app)
-    return { "unique_filename": unique_filename }
-
-
-@app.route("/su-file/<unique_filename>/filters", methods=['PUT'])
-def updateSuFile(unique_filename):
-    data = request.get_json()
-    updateOptions = data["updateOptions"]
-    process_output = seismicFileRepository.update(unique_filename, updateOptions)
-    return jsonify({
-        "process_output": process_output
-    })
- 
-
-@app.route("/get-plot/<unique_filename>", methods=['GET'])
-def showPlotHtmlTags(unique_filename):
-    script, div = plotRepository.show(unique_filename)
-    return jsonify({
-        "script": script,
-        "div": div
-    })
+app.register_blueprint(router)
---
+++ server/app/routes.py
@@ -0,0 +1,42 @@
+from flask import Blueprint, send_file, request, jsonify
+
+from .repositories.PlotRepository import PlotRepository
+from .repositories.SeismicFileRepository import SeismicFileRepository
+
+router = Blueprint("routes", __name__)
+
+plotRepository = PlotRepository()
+seismicFileRepository = SeismicFileRepository()
+
+@router.route("/su-file", methods=['GET'])
+def showSuFile():
+    try:
+        return send_file('../static/marmousi_CS.su')
+    except Exception as error:
+        return str(error)
+
+
+@router.route("/su-file", methods=['POST'])
+def createSuFile():
+    file = request.files['file']
+    unique_filename = seismicFileRepository.create(file)
+    return { "unique_filename": unique_filename }
+
+
+@router.route("/su-file/<unique_filename>/filters", methods=['PUT'])
+def updateSuFile(unique_filename):
+    data = request.get_json()
+    updateOptions = data["updateOptions"]
+    process_output = seismicFileRepository.update(unique_filename, updateOptions)
+    return jsonify({
+        "process_output": process_output
+    })
+ 
+
+@router.route("/get-plot/<unique_filename>", methods=['GET'])
+def showPlotHtmlTags(unique_filename):
+    script, div = plotRepository.show(unique_filename)
+    return jsonify({
+        "script": script,
+        "div": div
+    })
