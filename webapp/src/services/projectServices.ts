628615f23b7f37cd6216d140e7abb36646f3fb15
JorbFreire <jorbfreire@gmail.com>
2023-08-17 16:48:03 -0300

    fix: rename route

--- webapp/src/services/projectServices.ts
+++ webapp/src/services/projectServices.ts
@@ -2,20 +2,22 @@ import api from "./api"
 
 export async function getProjectsByUserID(userID: string): Promise<Array<IProject> | []> {
   try {
-    const response = await api.get<Array<IProject>>(`/seismic-projects/list/${userID}`)
+    const response = await api.get<Array<IProject>>(`/seismic-project/list/${userID}`)
     return response.data
   } catch (error) {
+    console.error(error)
     return []
   }
 }
 
 export async function createNewProject(userID: string, name: string): Promise<IProject | null> {
   try {
-    const response = await api.post<IProject>(`/seismic-projects/list/${userID}`, {
+    const response = await api.post<IProject>(`/seismic-project/create/${userID}`, {
       name
     })
     return response.data
   } catch (error) {
+    console.error(error)
     return null
   }
 }
