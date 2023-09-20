bbcc6fdfb4e0b377597aaf7bbd2362bbb4cf44ce
JorbFreire <jorbfreire@gmail.com>
2023-09-20 17:21:21 -0300

    extract configs

--- server/app/main.py
+++ server/app/main.py
@@ -2,18 +2,18 @@ from flask import Flask, jsonify
 from flask_cors import CORS
 from os import path, getcwd
 
+from .config.database import migrations_root_path, db_uri
+from .config.upload import upload_folder
 from .database.connection import database, migrate
 from .routes import router
 from .errors.AppError import AppError
 
-database_root_path = path.join(getcwd(), 'app/database')
-migrations_root_path = path.join(database_root_path, 'migrations')
 
 app = Flask(__name__)
 CORS(app)
 
-app.config['UPLOAD_FOLDER'] = "static"
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://admin:mysecretpassword@localhost:5432/admin"
+app.config['UPLOAD_FOLDER'] = upload_folder
+app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 database.init_app(app)
