e345877e693d9bb490eb0f4b2ff6ea8470452828
synthbe <pedrom.varelac@gmail.com>
2024-02-13 22:14:01 -0300

    dataset commands

--- server/app/models/DataSetModel.py
+++ server/app/models/DataSetModel.py
@@ -4,7 +4,9 @@ from typing import List
 
 from ..database.connection import database
 from .WorkflowParentsAssociationModel import WorkflowParentsAssociationModel
+from .OrderedCommandsListModel import OrderedCommandsListModel
 from .WorkflowModel import WorkflowModel
+from .CommandModel import CommandModel 
 
 
 class DataSetModel(database.Model):
@@ -21,8 +23,12 @@ class DataSetModel(database.Model):
         List[WorkflowParentsAssociationModel]
     ] = relationship(WorkflowParentsAssociationModel)
 
+    orderedCommands: Mapped[
+        List[OrderedCommandsListModel]
+    ] = relationship(OrderedCommandsListModel)
+
     def _getWorkflows(self) -> list[dict[str, str]]:
-        if len(self.workflowParentAssociations) is 0:
+        if len(self.workflowParentAssociations) == 0:
             return []
         workflows = WorkflowModel.query.filter(
             WorkflowModel.id.in_(
@@ -31,8 +37,17 @@ class DataSetModel(database.Model):
         ).all()
         return [workflow.getResumedAttributes() for workflow in workflows]
 
+    def _getCommands(self):
+        commands = CommandModel.query.filter(
+            CommandModel.id._in(
+                [command.workflowId for command in self.orderedCommands]
+            )
+        )
+        return [command.getAttributes() for command in commands]
+
     def getAttributes(self) -> dict:
         return {
             "id": self.id,
             "workflows": self._getWorkflows(),
+            "commands": self._getCommands()
         }
