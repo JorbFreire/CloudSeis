d15a8b166b5ba0f5267d447fffdc4c2be1b864fe
JorbFreire <jorbfreire@gmail.com>
2024-02-23 11:39:24 -0300

    remove decrapted route

--- server/app/routes/index.py
+++ server/app/routes/index.py
@@ -1,7 +1,6 @@
 from flask import Blueprint
 
 from .suFilesRouter import suFileRouter
-from .plotRouter import plotRouter
 from .userRouter import userRouter
 
 from .projectRouter import projectRouter
@@ -19,7 +18,6 @@ router = Blueprint("routes", __name__)
 
 # todo: add body data validator for each route
 router.register_blueprint(suFileRouter)
-router.register_blueprint(plotRouter)
 router.register_blueprint(userRouter)
 
 router.register_blueprint(projectRouter)
