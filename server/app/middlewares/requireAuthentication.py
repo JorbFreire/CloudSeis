5c2b8a4d3b2c443c2553a93857bb6906cc3cd302
JorbFreire <jorbfreire@gmail.com>
2024-03-19 17:40:38 -0300

    change comment

--- server/app/middlewares/requireAuthentication.py
+++ server/app/middlewares/requireAuthentication.py
@@ -34,8 +34,8 @@ def requireAuthentication(routeFunction, isAdminRequired=False):
         if isAdminRequired and not user["is_admin"]:
             raise AuthError("Must be admin")
 
-        # *** "payload.id" will be the first argument of any function using
-        # *** "requireAuthentication" using this as decorator
+        # *** "payload.id" will be the first argument of any function
+        # *** using "requireAuthentication" as decorator
         response = routeFunction(payload.id, *args, **kwargs)
         return response
     return wrapper
