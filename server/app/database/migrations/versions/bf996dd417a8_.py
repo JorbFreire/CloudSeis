ecf5dc16e889cfecbe82ec63a6dee99b697e9ae3
JorbFreire <jorbfreire@gmail.com>
2024-03-19 02:19:39 -0300

    fix double head migrations

---
+++ server/app/database/migrations/versions/bf996dd417a8_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: bf996dd417a8
+Revises: 29842d62934e, df2a8cb830f8
+Create Date: 2024-03-19 02:18:17.796226
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'bf996dd417a8'
+down_revision = ('29842d62934e', 'df2a8cb830f8')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
