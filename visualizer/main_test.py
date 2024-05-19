1e89a69bcb615d87adb9161e04040541f53f5385
cadu-sg <eduardo.garabito@hotmail.com>
2024-05-19 13:34:56 -0300

    minimal working

--- visualizer/main_test.py
+++ visualizer/main_test.py
@@ -1,7 +1,7 @@
 # bokeh serve step_6.py
 
 from bokeh.layouts import column, row
-from bokeh.models import Paragraph, RangeSlider, Switch, Slider, Spinner
+from bokeh.models import Paragraph, Slider, Spinner, Switch
 from bokeh.plotting import curdoc
 from seismicio import readsu
 
@@ -11,7 +11,7 @@ from seismic_visualization import SeismicVisualization
 # ---------------------
 file_path = "/storage1/Seismic/dados_teste/marmousi_4ms_CDP.su"
 gather_key = "cdp"
-num_loadedgathers: int = 5
+num_loadedgathers: int = 2
 start_loadedgathers: int = 0  # zero-based indexing
 
 # Ler dado sísmico
@@ -26,11 +26,33 @@ interval_time_samples = sufile.headers.dt[0] / 1000000  # µs → s
 # SeismicVisualization
 # --------------------
 seismic_visualization = SeismicVisualization(
-    data=sufile.gather[start_loadedgathers:start_loadedgathers + num_loadedgathers].data,
+    data=sufile.igather[start_loadedgathers : start_loadedgathers + num_loadedgathers].data,
     x_positions=None,
     interval_time_samples=interval_time_samples,
 )
 num_gathers = sufile.num_gathers
+
+simple_paragraph = Paragraph(text="Gathers")
+
+
+def update_information(gather_index_start: int, gather_index_stop: int):
+    gather_value_start = sufile.gather_index_to_value(gather_index_start)
+    gather_value_end = sufile.gather_index_to_value(gather_index_stop)
+    simple_paragraph.text = f"Gather {gather_value_start} to {gather_value_end}"
+
+
+update_information(start_loadedgathers, start_loadedgathers + num_loadedgathers)
+
+
+def update_plotting(igather_start: int, igather_stop: int):
+    # This function expects the index or slice to be correct
+    print(f"PLOT igather[{igather_start}:{igather_stop}]")
+    seismic_visualization.update_plot(
+        data=sufile.igather[igather_start:igather_stop].data,
+        x_positions=None,
+        interval_time_samples=interval_time_samples,
+    )
+    update_information(igather_start, igather_stop)
 
 
 def range_slider_input_handler(attr, old, new):
@@ -45,29 +67,39 @@ def range_slider_input_handler(attr, old, new):
 
 
 def spinner_value_callback(attr, old, new):
-    global num_loadedgathers
+    global start_loadedgathers, num_loadedgathers, spinner
+    spinner.disabled = True
+    slider.disabled = True
+
     num_loadedgathers = round(new)
-    handle_loadedgathers_update()
+
+    stop_loadedgathers = start_loadedgathers + num_loadedgathers
+    # if exceeding to the right
+    if stop_loadedgathers > num_gathers:
+        stop_loadedgathers = num_gathers
+        num_loadedgathers = stop_loadedgathers - start_loadedgathers + 1
+        spinner.value = num_loadedgathers
+
+    update_plotting(start_loadedgathers, stop_loadedgathers)
+    spinner.disabled = False
+    slider.disabled = False
 
 
 def slider_value_callback(attr, old, new):
-    global start_loadedgathers, slider
+    global start_loadedgathers, num_loadedgathers, slider
+    spinner.disabled = True
+    slider.disabled = True
     start_loadedgathers = round(new) - 1
-
     stop_loadedgathers = start_loadedgathers + num_loadedgathers
-
-    # exceed to the right
+    # if exceeding to the right
     if stop_loadedgathers > num_gathers:
         start_loadedgathers = num_gathers - num_loadedgathers
         stop_loadedgathers = num_gathers
         slider.value = start_loadedgathers
 
-    
-    seismic_visualization.update_plot()
-
-
-def handle_loadedgathers_update():
-    pass
+    update_plotting(start_loadedgathers, stop_loadedgathers)
+    spinner.disabled = False
+    slider.disabled = False
 
 
 # Widgets
@@ -80,16 +112,16 @@ spinner = Spinner(
     low=1,
     high=sufile.num_gathers,
     step=1,
-    value=start_loadedgathers,
+    value=num_loadedgathers,
 )
 spinner.on_change("value_throttled", spinner_value_callback)
 
 slider = Slider(
     start=1,
     end=sufile.num_gathers,
-    value=num_loadedgathers,
+    value=start_loadedgathers + 1,
     step=1,
-    title="Gather sequential number",
+    title="Gather sequential number start",
     sizing_mode="stretch_width",
 )
 slider.on_change("value_throttled", slider_value_callback)
@@ -102,6 +134,7 @@ seismic_visualization.js_link_image_visible(switch_image, "active")
 seismic_visualization.assign_harea_switch(switch_areas)
 
 left_tools_column = column(
+    simple_paragraph,
     row(Paragraph(text="Image"), switch_image),
     row(Paragraph(text="Lines"), switch_lines),
     row(Paragraph(text="Areas"), switch_areas),
