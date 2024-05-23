537b9ea0551041250ea6e8a25df8e52541e509af
JorbFreire <jorbfreire@gmail.com>
2024-05-23 01:21:36 -0300

    fix: userId param

--- server/app/routes/datasetRouter.py
+++ server/app/routes/datasetRouter.py
@@ -5,7 +5,6 @@ from ..models.WorkflowModel import WorkflowModel
 from ..models.DataSetModel import DataSetModel
 
 from ..repositories.DatasetRepository import DatasetRepository
-from ..errors.AppError import AppError
 
 from ..middlewares.requireAuthentication import requireAuthentication
 from ..middlewares.decoratorsFactory import decorator_factory
@@ -14,10 +13,9 @@ datasetRouter = Blueprint("dataset-routes", __name__, url_prefix="/dataset")
 datasetRepository = DatasetRepository()
 
 
-@datasetRouter.route("/create/<workflowId>", methods=['POST'])
 # todo: final version should not get a "create" route for dataset
 # todo: datasets should be created when running plot
-# todo: make validations
+@datasetRouter.route("/create/<workflowId>", methods=['POST'])
 @decorator_factory(requireAuthentication, routeModel=WorkflowModel)
 def createDataset(_, workflowId):
     data = request.get_json()
@@ -27,17 +25,17 @@ def createDataset(_, workflowId):
     )
     return jsonify(newDataset)
 
-# Delete method, not sure
+
 @datasetRouter.route("/delete/<id>", methods=['DELETE'])
 @decorator_factory(requireAuthentication, routeModel=DataSetModel)
-def deleteDataset(id):
+def deleteDataset(_, id):
     dataset = datasetRepository.delete(id)
     return jsonify(dataset)
 
 
 @datasetRouter.route("/show/<id>", methods=['GET'])
 @decorator_factory(requireAuthentication, routeModel=DataSetModel)
-def showDataset(id):
+def showDataset(_, id):
     dataset = datasetRepository.showById(id)
     return jsonify(dataset)
 
