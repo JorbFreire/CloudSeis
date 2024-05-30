999f95d43fa08cd6579031c38fa72092c2f46a60
cadu-sg <eduardo.garabito@hotmail.com>
2024-05-30 13:49:01 -0300

    done

--- seismic-webviz/seismic_visualization.py
+++ seismic-webviz/seismic_visualization.py
@@ -20,42 +20,21 @@ class SeismicVisualization:
         data: npt.NDArray,
         x_positions: npt.NDArray | None,
         interval_time_samples: float,
-        time_unit="s",
-        stretch_factor=0.15,
-        color="black",
+        time_unit: str = "s",
+        stretch_factor: float = 0.15,
+        color: str = "black",
+        gather_key: str | None = None,
     ):
-        init_start = perf_counter()
         # Input checks
         # ------------
-
-        # Input check for stretch_factor
-        if not isinstance(stretch_factor, (int, float)):
-            raise TypeError("stretch_factor must be a number")
-
-        # Input check for data
-        if type(data).__module__ != np.__name__:
-            raise TypeError("data must be a numpy array")
-        if len(data.shape) != 2:
-            raise ValueError("data must be a 2D array")
-
+        self._check_stretch_factor(stretch_factor)
+        self._check_data(data)
         num_time_samples = data.shape[0]
         num_traces = data.shape[1]
-        print("num_traces:", num_traces)
-
-        # Input check for x_positions
         if x_positions is None:
             x_positions = np.arange(start=1, stop=num_traces + 1)
         else:
-            if type(x_positions).__module__ != np.__name__:
-                raise TypeError("x_positions must be a numpy array")
-            if len(x_positions.shape) != 1:
-                raise ValueError("x_positions must be a 1D array")
-            if x_positions.size != num_traces:
-                raise ValueError(
-                    "The size of x_positions must be equal to the number of "
-                    "columns in data, that is, it must be equal to the number "
-                    "of traces"
-                )
+            self._check_x_positions(x_positions, num_traces)
 
         # Create and set up figure object
         # -------------------------------
@@ -72,91 +51,74 @@ class SeismicVisualization:
         self.plot.y_range.range_padding = 0.0
         self.plot.y_range.flipped = True
 
-        # Adjust axes
-        self.plot.xaxis.axis_label = "Offset (m)"
+        # Adjust axes labels
+        if gather_key:
+            self.plot.xaxis.axis_label = gather_key
+        else:
+            self.plot.xaxis.axis_label = "trace sequential number"
         if time_unit == "s":
             self.plot.yaxis.axis_label = "Time (s)"
         elif time_unit == "ms":
             self.plot.yaxis.axis_label = "Time (ms)"
 
-        # Create ColumnDataSource objects and add renderers
-        # -------------------------------------------------
+        # Amplitudes rescaled (data for wiggle renderers)
+        data_rescaled = self._rescale_data(data, x_positions, stretch_factor)
 
-        # Time sample instants (auxiliary data for all)
+        # Time sample instants (data for all renderers)
         first_time_sample = 0.0
         last_time_sample = first_time_sample + (num_time_samples - 1) * interval_time_samples
         time_sample_instants = np.linspace(
             start=first_time_sample, stop=last_time_sample, num=num_time_samples
         )
 
+        # Create ColumnDataSource objects
+        # -------------------------------
+
         # Create image source
         self.image_source = ColumnDataSource(data={"image": [data]})
 
-        # Auxiliary data for image renderer parameters
-        width_x_positions = np.abs(x_positions[0] - x_positions[-1])
-        width_time_sample_instants = np.abs(time_sample_instants[0] - time_sample_instants[-1])
-        distance_first_x_positions = x_positions[1] - x_positions[0]
-        distance_last_x_positions = x_positions[-1] - x_positions[-2]
-
-        # Add image renderer
-        self.image_renderer: GlyphRenderer = self.plot.image(
-            image="image",
-            source=self.image_source,
-            x=x_positions[0] - distance_first_x_positions / 2,
-            y=first_time_sample,
-            dw=width_x_positions + (distance_first_x_positions + distance_last_x_positions) / 2,
-            dh=width_time_sample_instants,
-            palette="Greys256",
-            anchor="bottom_left",
-            origin="bottom_left",
-        )
-
-        # Amplitudes zeros
-        amplitudes_zeros = np.zeros(shape=(num_time_samples,))
-
-        # Minimum trace horizontal spacing
-        trace_x_spacing = np.min(np.diff(x_positions))
-
-        # Rescale data by trace_x_spacing and stretch_factor
-        data_max_std = np.max(np.std(data, axis=0))
-        data_rescaled = data / data_max_std * trace_x_spacing * stretch_factor
-
-        xs_list = []
-        ys_list = []
-        self.harea_gl_list: list[GlyphRenderer] = []
-
-        for trace_index in range(num_traces):
-            x_position = x_positions[trace_index]
-            amplitudes = data_rescaled[:, trace_index]
-
-            # fill positive amplitudes
-            amplitudes_positive = np.clip(amplitudes, a_min=0, a_max=None)
-            # Call harea glyph renderer
-            # self.harea_gl_list.append(
-
-            # Add harea glyph renderer
-            if num_traces <= MAX_TRACES_LINE_HAREA:
-                self.plot.harea(
-                    x1=amplitudes_zeros + x_position,
-                    x2=amplitudes_positive + x_position,
-                    y=time_sample_instants,
-                    color="black",
-                    name="H",
-                )
-
-            # construct CDS for multi_line render
-            xs_list.append(amplitudes + x_position)
-            ys_list.append(time_sample_instants)
-
+        # Create multi_line renderer's source
         self.multi_line_source = ColumnDataSource(
-            data={
-                "xs": xs_list,
-                "ys": ys_list,
-            }
+            data=self._compute_multi_line_source_data(data_rescaled, x_positions, time_sample_instants)
         )
 
-        # Add multiline renderer
-        self.line_renderer: GlyphRenderer = self.plot.multi_line(
+        # Add renderers
+        # -------------
+
+        # --- Add (single) image renderer ---
+        # auxiliary data for image renderer parameters
+        width_time_sample_instants = np.abs(time_sample_instants[0] - time_sample_instants[-1])
+        if num_traces == 1:
+            self.image_renderer: GlyphRenderer = self.plot.image(
+                image="image",
+                source=self.image_source,
+                x=x_positions[0] - 1,
+                y=first_time_sample,
+                dw=2,
+                dh=width_time_sample_instants,
+                palette="Greys256",
+                anchor="bottom_left",
+                origin="bottom_left",
+            )
+        else:
+            # more auxiliary data for image renderer parameters
+            width_x_positions = np.abs(x_positions[0] - x_positions[-1])
+            distance_first_x_positions = x_positions[1] - x_positions[0]
+            distance_last_x_positions = x_positions[-1] - x_positions[-2]
+            self.image_renderer: GlyphRenderer = self.plot.image(
+                image="image",
+                source=self.image_source,
+                x=x_positions[0] - distance_first_x_positions / 2,
+                y=first_time_sample,
+                dw=width_x_positions + (distance_first_x_positions + distance_last_x_positions) / 2,
+                dh=width_time_sample_instants,
+                palette="Greys256",
+                anchor="bottom_left",
+                origin="bottom_left",
+            )
+
+        # Add (single) multi_line renderer
+        self.multi_line_renderer: GlyphRenderer = self.plot.multi_line(
             xs="xs",
             ys="ys",
             source=self.multi_line_source,
@@ -164,10 +126,10 @@ class SeismicVisualization:
             visible=self.line_widget.active if self.line_widget else True,
         )
 
-        self._set_up_renderers_on_trace_excess(num_traces)
+        # Add (multiple) harea renderers
+        self._add_hareas(data_rescaled, x_positions, time_sample_instants)
 
-        init_stop = perf_counter()
-        print(f"\ninit time: {init_stop - init_start} seconds\n")
+        self._set_up_renderers_on_trace_excess(num_traces)
 
     @staticmethod
     def _check_stretch_factor(stretch_factor):
@@ -296,6 +258,7 @@ class SeismicVisualization:
         time_unit="s",
         stretch_factor=0.15,
         color="black",
+        gather_key: str | None = None,
     ):
         # Input checks
         # ------------
@@ -311,10 +274,10 @@ class SeismicVisualization:
         # Hold off all requests to repaint the plot
         self.plot.hold_render = True
 
-        # Data after rescaling
+        # Amplitudes rescaled (data for wiggle renderers)
         data_rescaled = self._rescale_data(data, x_positions, stretch_factor)
 
-        # Time sample instants
+        # Time sample instants (data for all renderers)
         first_time_sample = 0.0
         last_time_sample = first_time_sample + (num_time_samples - 1) * interval_time_samples
         time_sample_instants = np.linspace(
@@ -338,8 +301,11 @@ class SeismicVisualization:
 
         # Update plot setup
         # -----------------
-        # Adjust axes
-        self.plot.xaxis.axis_label = "Offset (m)"
+        # Adjust axes labels
+        if gather_key:
+            self.plot.xaxis.axis_label = gather_key
+        else:
+            self.plot.xaxis.axis_label = "trace sequential number"
         if time_unit == "s":
             self.plot.yaxis.axis_label = "Time (s)"
         elif time_unit == "ms":
@@ -383,7 +349,7 @@ class SeismicVisualization:
             if self.line_widget:
                 self.line_widget.active = False
             else:
-                self.line_renderer.visible = False
+                self.multi_line_renderer.visible = False
             # reveal image renderer
             if self.image_widget:
                 self.image_widget.active = True
@@ -394,14 +360,14 @@ class SeismicVisualization:
             if self.area_widget is not None:
                 self.area_widget.disabled = False
 
-    def assign_line_widget(self, switch: Switch):
+    def assign_line_switch(self, switch: Switch):
         """Link the `active` attribute of a Switch instance (either a
         Switch or CheckBox instance) to the visibility of the line renderer"""
         self.line_widget = switch
-        self.line_widget.active = self.line_renderer.visible
-        self.line_widget.js_link("active", self.line_renderer, "visible")
+        self.line_widget.active = self.multi_line_renderer.visible
+        self.line_widget.js_link("active", self.multi_line_renderer, "visible")
 
-    def assign_area_widget(self, switch: Switch):
+    def assign_area_switch(self, switch: Switch):
         """Link the `active` attribute of a Switch instance (either a
         Switch or CheckBox instance) to the visibility of the area renderer"""
 
@@ -415,7 +381,7 @@ class SeismicVisualization:
         self.area_widget = switch
         self.area_widget.on_change("active", harea_switch_handler)
 
-    def asssign_image_widget(self, switch: Switch):
+    def asssign_image_switch(self, switch: Switch):
         """Link the `active` attribute of a Switch instance (either a
         Switch or CheckBox instance) to the visibility of the image renderer"""
         self.image_widget = switch
