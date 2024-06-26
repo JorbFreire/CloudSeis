433f525a90ae8f077361ea0eae041a37fcdb1c45
JorbFreire <jorbfreire@gmail.com>
2024-06-26 19:04:03 -0300

    change lines state machine

---
+++ webapp/src/store/LinesStore.ts
@@ -0,0 +1,60 @@
+import { create } from 'zustand'
+
+import { getLinesByProjectID } from 'services/lineServices'
+import { createNewLine } from 'services/lineServices'
+import { createNewWorkflow } from 'services/workflowServices'
+
+type linesType = Array<ILine>
+
+interface ILinesStoreState {
+  lines: linesType
+  loadLines: (projectId: number) => void
+  pushNewWorkflowToLine: (lineId: number, name: string) => void
+  saveNewLine: (projectId: number, name: string) => void
+}
+
+export const useLinesStore = create<ILinesStoreState>((set) => ({
+  lines: [],
+  loadLines: (projectId: number) => {
+    const token = localStorage.getItem("jwt")
+    if (!token)
+      return
+    getLinesByProjectID(token, projectId)
+      .then((result) => {
+        if (result === 401)
+          return
+
+        if (Array.isArray(result))
+          set({ lines: result })
+      })
+  },
+  saveNewLine: (projectId: number, name: string) => {
+    const token = localStorage.getItem("jwt")
+    if (!token)
+      return
+    createNewLine(token, projectId, name)
+      .then(result => {
+        if (!result)
+          return
+        set((state) => ({ lines: [...state.lines, result] }))
+      })
+  },
+  pushNewWorkflowToLine: (lineId: number, name: string) => {
+    const token = localStorage.getItem("jwt")
+    if (!token)
+      return
+
+    createNewWorkflow(token, lineId, "lineId", name)
+      .then(result => {
+        if (!result)
+          return
+        set((state) => ({
+          lines: state.lines.map((line) => {
+            if (line.id == lineId)
+              line.workflows.push(result)
+            return line
+          })
+        }))
+      })
+  }
+}))
