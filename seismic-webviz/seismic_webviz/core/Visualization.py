adf845089af3de317c7d96b85d4e172e5ed1d292
JorbFreire <jorbfreire@gmail.com>
2024-07-09 13:21:06 -0300

    fix: missing parameter

--- seismic-webviz/seismic_webviz/core/Visualization.py
+++ seismic-webviz/seismic_webviz/core/Visualization.py
@@ -3,6 +3,6 @@ from . import MultiGatherVisualization, StackVisualization
 class Visualization(MultiGatherVisualization, StackVisualization):
 	def __init__(self, filename: str, gather_key: str | None = None):
 		if gather_key:
-			MultiGatherVisualization.__init__(filename, gather_key)
+			MultiGatherVisualization.__init__(self, filename, gather_key)
 		else:
-			StackVisualization.__init__(filename)
+			StackVisualization.__init__(self, filename)
