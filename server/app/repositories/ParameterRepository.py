f3524d74f7d2d09e66664f128d7fc1debd964edd
synthbe <108361383+synthbe@users.noreply.github.com>
2024-12-30 15:49:39 -0300

    typing added ParameterRepository file

--- server/app/repositories/ParameterRepository.py
+++ server/app/repositories/ParameterRepository.py
@@ -5,32 +5,33 @@ from ..errors.AppError import AppError
 
 
 class ParameterRepository:
-    def showByProgramId(self, programId):
+    def showByProgramId(self, programId: int | str) -> list[dict[str, str | int | bool]]:
         parameters = ParameterModel.query.filter_by(programId=programId).all()
         if not parameters:
             raise AppError("There are no parameters for this program", 404)
 
         return [parameter.getAttributes() for parameter in parameters]
 
-    def create(self, programId):
+    def create(self, programId: int | str) -> dict[str, str | int | bool]:
         program = ProgramModel.query.filter_by(
             id=programId
         ).first()
         if not program:
             raise AppError("Program does not exist", 404)
 
-        newProgram = ParameterModel(
+        newParameter = ParameterModel(
             name="",
             description="",
+            examples="",
             input_type="",
             isRequired=False,
             programId=programId
         )
-        database.session.add(newProgram)
+        database.session.add(newParameter)
         database.session.commit()
-        return newProgram.getAttributes()
+        return newParameter.getAttributes()
 
-    def update(self, parameterId, parameterNewData):
+    def update(self, parameterId: int | str, parameterNewData: dict[str, str]) -> dict[str, str | int | bool]:
         parameter = ParameterModel.query.filter_by(
             id=parameterId
         ).first()
@@ -39,13 +40,14 @@ class ParameterRepository:
 
         parameter.name = parameterNewData["name"]
         parameter.description = parameterNewData["description"]
+        parameter.example = parameterNewData["example"]
         parameter.input_type = parameterNewData["input_type"]
         parameter.isRequired = parameterNewData["isRequired"]
 
         database.session.commit()
         return parameter.getAttributes()
 
-    def delete(self, id):
+    def delete(self, id: str | int) -> dict[str, str | int | bool]:
         parameter = ParameterModel.query.filter_by(id=id).first()
         if not parameter:
             raise AppError("Parameter does not exist", 404)
