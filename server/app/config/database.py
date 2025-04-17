e8b7e99d6a8173c6c147efa287c4657f0dcc1e97
JorbFreire <jorbfreire@gmail.com>
2025-04-17 15:48:26 -0300

    fix db enviorements

--- server/app/config/database.py
+++ server/app/config/database.py
@@ -5,25 +5,25 @@ from ._checkForEnvValue import checkForEnvValue
 database_root_path = path.join(getcwd(), 'app/database')
 migrations_root_path = path.join(database_root_path, 'migrations')
 
-modeKeyType = Literal["production", "development", "test"]
+modeKeyType = Literal["PRODUCTION", "DEVELOPMENT", "TEST"]
 
 def get_db_uri(mode: modeKeyType) -> str:
     dbConfigOptions = {
-        "production": {
+        "PRODUCTION": {
             "dbname": getenv("DATABASE_NAME"),
             "user": getenv("DATABASE_USER"),
             "password": getenv("DATABASE_PASSWORD"),
             "host": getenv("DATABASE_HOST"),
             "port": getenv("DATABASE_PORT")
         },
-        "development": {
+        "DEVELOPMENT": {
             "dbname": checkForEnvValue("DATABASE_NAME", "admin"),
             "user": checkForEnvValue("DATABASE_USER", "admin"),
             "password": checkForEnvValue("DATABASE_PASSWORD", "mysecretpassword"),
             "host": checkForEnvValue("DATABASE_HOST", "localhost"),
             "port": checkForEnvValue("DATABASE_PORT", "5432")
         },
-        "test": {
+        "TEST": {
             "dbname": checkForEnvValue("DATABASE_NAME", "admin"),
             "user": checkForEnvValue("DATABASE_USER", "admin"),
             "password": checkForEnvValue("DATABASE_PASSWORD", "mysecretpassword"),
